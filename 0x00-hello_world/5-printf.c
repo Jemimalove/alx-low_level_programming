79fc5ba5b40248d924b35870bec0d5131eec5ea1
Jemimalove <riquirays@gmail.com>
2022-06-30 22:04:05 +0200

    C code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - entry point of the program
+*
+*Description: A program that prints
+*Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+	
+}
