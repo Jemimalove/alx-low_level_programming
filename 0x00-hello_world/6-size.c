41a2179df244657d673f8065e3a55f709122aac0
Jemimalove <riquirays@gmail.com>
2022-06-30 22:23:43 +0200

    First commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
 *main -entry point of the program
 *
@@ -8,10 +9,10 @@
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	printf("Size of an int: byte(s)\n", (int) sizeof(int));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("Size 0f a long int: %d byte(s)\n",(int) sizeof(long int);
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-	printf("Size of a flot: %d byte(s)\n", (int) sizeof(float));
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 
 	return (0);
 }
