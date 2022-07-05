c1b69c119d547fc9f86f3b084b30c4675d855f63
Jemimalove <riquirays@gmail.com>
2022-07-05 18:51:41 +0200

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,23 +2,23 @@
 
 /**
  * main - printfs Putchar
- * 
+ *
  * Description: prints _puntchar
- * 
+ *
  * Return: 0 (success)
  */
 
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    
-    return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
